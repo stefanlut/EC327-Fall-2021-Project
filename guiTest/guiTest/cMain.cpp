bf2ce34cdaa1ee04ecfd66ae4f0d09cac7e26095
stefanlut <slutschg@bu.edu>
2021-12-03 17:15:15 -0500

    I hope this will commit, adding buttons but no functionality  yet

--- guiTest/guiTest/cMain.cpp
+++ guiTest/guiTest/cMain.cpp
@@ -1,7 +1,9 @@
 #include "cMain.h"
-cMain::cMain() : wxFrame(nullptr,wxID_ANY,"TestGUI - EC327")
+cMain::cMain() : wxFrame(nullptr,wxID_ANY,"TestGUI - EC327",wxPoint(30,30),wxSize(800,600))
 {
-
+	m_btn1 = new wxButton(this, 10001, "Yes", wxPoint(200, 300), wxSize(50, 20));
+	m_btn2 = new wxButton(this, 10002, "No", wxPoint(400, 300), wxSize(50, 20));
+	m_text = new wxStaticText(this, wxID_ANY, "Have you experienced a new onset of any of the following symptoms in the past 14 days?\nFever of 100°F or feeling unusually hot with shivering / chills, sore throat, new cough not related to a chronic condition, difficulty breathing / shortness of breath, severe fatigue, severe muscle aches, new loss of sense of taste or smell ? ", wxPoint(250, 100), wxSize(200, 200));
 }
 cMain::~cMain()
 {
--- guiTest/guiTest/cMain.h
+++ guiTest/guiTest/cMain.h
@@ -1,9 +1,13 @@
 #pragma once
 #include "wx/wx.h"
+#include "wx/stattext.h"
 class cMain : public wxFrame
 {
 public:
 	cMain();
 	~cMain();
+public:
+	wxButton* m_btn1 = nullptr, * m_btn2 = nullptr;
+	wxStaticText* m_text = nullptr;
 };
 
