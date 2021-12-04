b34c42609435daf78e7de61268f46c0de9d5c5ce
stefanlut <slutschg@bu.edu>
2021-12-04 14:56:29 -0500

    Adding functionality to buttons

--- guiTest/guiTest/cMain.h
+++ guiTest/guiTest/cMain.h
@@ -3,11 +3,18 @@
 #include "wx/stattext.h"
 class cMain : public wxFrame
 {
+private:
+	bool has_COVID;
+	bool button_Pressed = false;
 public:
 	cMain();
 	~cMain();
 public:
-	wxButton* m_btn1 = nullptr, * m_btn2 = nullptr;
+	wxButton* m_btn1 = nullptr, * m_btn2 = nullptr, * m_btn3 = nullptr;
 	wxStaticText* m_text = nullptr;
+	void OnYesButtonClicked(wxCommandEvent& evt);
+	void OnNoButtonClicked(wxCommandEvent& evt);
+	void OnSaveButtonClicked(wxCommandEvent& evt);
+	wxDECLARE_EVENT_TABLE();
 };
 
