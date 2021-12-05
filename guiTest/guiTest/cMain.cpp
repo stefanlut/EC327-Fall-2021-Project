c4bb1e83034d933b604c143a7de42f7309eebc2d
stefanlut <slutschg@bu.edu>
2021-12-04 19:00:07 -0500

    Dialog appears when you save results.

--- guiTest/guiTest/cMain.cpp
+++ guiTest/guiTest/cMain.cpp
@@ -14,6 +14,7 @@ cMain::cMain() : wxFrame(nullptr,wxID_ANY,"TestGUI - EC327",wxPoint(30,30),wxSiz
 	m_btn2 = new wxButton(this, 10002, "No", wxPoint(400, 300), wxSize(50, 20));
 	m_btn3 = new wxButton(this, 10003, "Save Response to .txt file", wxPoint(250, 400), wxSize(150, 20));
 	m_text = new wxStaticText(this, wxID_ANY, "Have you experienced a new onset of any of the following symptoms in the past 14 days?\nFever of 100°F or feeling unusually hot with shivering / chills, sore throat, new cough not related to a chronic condition, difficulty breathing / shortness of breath, severe fatigue, severe muscle aches, new loss of sense of taste or smell ? ", wxPoint(250, 100), wxSize(200, 200));
+	m_msg = new wxMessageDialog(this, "Results Saved!", "Results Confirmation", wxOK | wxCENTRE, wxDefaultPosition);
 }
 cMain::~cMain()
 {
@@ -24,12 +25,14 @@ void cMain::OnYesButtonClicked(wxCommandEvent& evt)
 {
 	has_COVID = true;
 	button_Pressed = true;
+	evt.Skip();
 }
 
 void cMain::OnNoButtonClicked(wxCommandEvent& evt)
 {
 	has_COVID = false;
 	button_Pressed = true;
+	evt.Skip();
 }
 
 void cMain::OnSaveButtonClicked(wxCommandEvent& evt)
@@ -39,13 +42,14 @@ void cMain::OnSaveButtonClicked(wxCommandEvent& evt)
 		std::ofstream resultsFile("example.txt"); // This filename should probably be something like Day_Month_Year_Results.txt
 		resultsFile << "Has COVID-19: " << "Yes" << std::endl;
 		resultsFile.close();
+		m_msg->ShowModal();
 	}
 	else if (button_Pressed && !has_COVID)
 	{
 		std::ofstream resultsFile("example.txt"); // This filename should probably be something like Day_Month_Year_Results.txt
 		resultsFile << "Has COVID-19: " << "No" << std::endl;
 		resultsFile.close();
+		m_msg->ShowModal();
 	}
-	else return;
-	
+	evt.Skip();
 }
--- guiTest/guiTest/cMain.h
+++ guiTest/guiTest/cMain.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "wx/wx.h"
 #include "wx/stattext.h"
+#include "wx/msgdlg.h"
 class cMain : public wxFrame
 {
 private:
@@ -12,6 +13,7 @@ public:
 public:
 	wxButton* m_btn1 = nullptr, * m_btn2 = nullptr, * m_btn3 = nullptr;
 	wxStaticText* m_text = nullptr;
+	wxMessageDialog* m_msg = nullptr;
 	void OnYesButtonClicked(wxCommandEvent& evt);
 	void OnNoButtonClicked(wxCommandEvent& evt);
 	void OnSaveButtonClicked(wxCommandEvent& evt);
