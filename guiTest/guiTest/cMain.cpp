295ac18e8797f541600f9c536c22deb733f471f1
PotofGross <agross13@bu.edu>
2021-12-08 18:36:50 -0500

    YEP Choices

--- guiTest/guiTest/cMain.cpp
+++ guiTest/guiTest/cMain.cpp
@@ -18,6 +18,10 @@ EVT_BUTTON(10013, OnExit2)
 wxEND_EVENT_TABLE()
 bool cond;
 int global = 1;
+wxString dateArray[7] = { wxString("Monday"),wxString("Tuesday"),wxString("Wednesday"),wxString("Thursday"),wxString("Friday"),wxString("Saturday"),wxString("Sunday") };
+wxString locArray[2] = { wxString("808 Commonwealth Ave."),wxString("BU Medical Center") };
+wxString Services[35] = { wxString("Tooth - Color Fillings"), wxString("Major Restorative"), wxString("Dentures, Partials, Bridges"), wxString("Bleaching"), wxString("Bonding"), wxString("Porcelain Veneers"), wxString("Implants"), wxString("Periodontics"), wxString("Oral Surgery"), wxString("Covid Test"), wxString("Covid Booster Shot"), wxString("Covid Vaccine"), wxString("Covid Isolation"), wxString("Covid Guidelines"), wxString("Covid Health Service"), wxString("Child Covid Care"),wxString("Vision Care"), wxString("Eye Exam"), wxString("Vision Tests"), wxString("Prescribe Glasses"), wxString("Contact Lenses"), wxString("Monitor Eye Condition"), wxString("Dry Eyes"), wxString("Glaucoma"), wxString("Vision Theraphy"), wxString("Provide Low Vision Aid"), wxString("New Family Consulation"), wxString("Newborn Care"), wxString("Immunizations"), wxString("Pediatric Nutrition"), wxString("Physical Exam"), wxString("Toddler Care"), wxString("FAN Club"), wxString("Obesity"), wxString("Fevers, Cold, Flu")};
+
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "TestGUI - EC327", wxPoint(30, 30), wxSize(800, 750))
 {
 	if (global == 1) {
@@ -38,6 +42,7 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "TestGUI - EC327", wxPoint(30, 30), 
 		m_btn5 = new wxButton(this, 10005, "Schedule Now", wxPoint(450, 10), wxSize(100, 25));
 		m_btn5->SetBackgroundColour(wxColor(115, 147, 179));
 		m_text1->SetBackgroundColour(wxColor(255, 00, 00));
+		m_msg5 = new wxMessageDialog(this, "Symptom(s) have been saved! \nYou may close the tab now.", "Results Confirmation", wxOK | wxCENTRE, wxDefaultPosition);
 		m_MenuBar = new wxMenuBar();
 		this->SetMenuBar(m_MenuBar);
 
@@ -53,12 +58,12 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "TestGUI - EC327", wxPoint(30, 30), 
 	if (global == 2) {
 		m_text1 = new wxStaticText(this, wxID_ANY, "Covid Vaccine Booster shot Appointment Registration", wxPoint(25, 10), wxSize(400, 50));
 		m_text1->SetBackgroundColour(wxColor(115, 147, 179));
-		m_txt3 = new wxTextCtrl(this, wxID_ANY, "Enter A Date (M,T,W Th, F): ", wxPoint(50, 100), wxSize(200, 50));
+		m_txt3 = new wxTextCtrl(this, wxID_ANY, "Enter A Date (S,M,T,W,Th,F,Sat): ", wxPoint(50, 100), wxSize(200, 50));
 		m_txt3->SetBackgroundColour(wxColor(115, 147, 179));
-		m_txt4 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(300, 100), wxSize(150, 50));
+		chc_date = new wxChoice(this, wxID_ANY, wxPoint(300, 100), wxDefaultSize,7,dateArray,0,wxDefaultValidator,"Weekday");
 		m_txt5 = new wxTextCtrl(this, wxID_ANY, "Enter A Location:\n(808 OR BUMC):", wxPoint(50, 200), wxSize(200, 50));
 		m_txt5->SetBackgroundColour(wxColor(115, 147, 179));
-		m_txt6 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(300, 200), wxSize(150, 50));
+		chc_loc = new wxChoice(this, wxID_ANY, wxPoint(300, 200), wxDefaultSize, 2, locArray, 0, wxDefaultValidator, "Location");
 		m_txt7 = new wxTextCtrl(this, wxID_ANY, "Enter your name & ID:", wxPoint(50, 300), wxSize(200, 50));
 		m_txt7->SetBackgroundColour(wxColor(115, 147, 179));
 		m_txt8 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(300, 300), wxSize(150, 50));
@@ -72,6 +77,7 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "TestGUI - EC327", wxPoint(30, 30), 
 		m_btn8->SetBackgroundColour(wxColor(115, 147, 179));
 		m_msg3 = new wxMessageDialog(this, "Saved! You May close the tab now.", "Results Confirmation", wxOK | wxCENTRE, wxDefaultPosition);
 		m_msg4 = new wxMessageDialog(this, "Please fill all forms.", "Results Confirmation", wxOK | wxCENTRE, wxDefaultPosition);
+		m_msg6 = new wxMessageDialog(this, "Please fill all forms with appropriate entries.", "Results Confirmation", wxOK | wxCENTRE, wxDefaultPosition);
 	}
 	if (global == 3) 
 	{
@@ -97,13 +103,13 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "TestGUI - EC327", wxPoint(30, 30), 
 		st3 = new wxStaticText(this, wxID_ANY, "Child Health Service:", wxPoint(600, 10), wxSize(200, 100));
 		st3->SetBackgroundColour(wxColor(144, 238, 144));
 		st4 = new wxStaticText(this, wxID_ANY,
-			"Dental Service:\n\n*Tooth-Color Fillings\n\n*Major Restorative\n\n*Dentures, Partials, Bridges\n\n*Bleaching\n\n*Bonding\n\n*Porcelain Veneers\n\n*Implants\n\n*Periodontics\n\n*Oral Surgery", wxPoint(0, 150), wxSize(200, 300));
+			"*Tooth-Color Fillings\n\n*Major Restorative\n\n*Dentures, Partials, Bridges\n\n*Bleaching\n\n*Bonding\n\n*Porcelain Veneers\n\n*Implants\n\n*Periodontics\n\n*Oral Surgery", wxPoint(0, 150), wxSize(200, 300));
 		st4->SetBackgroundColour(wxColor(247, 191, 190));
-		st5 = new wxStaticText(this, wxID_ANY, "Covid Service:\n\n*Covid Test\n\n*Covid Booster Shot\n\n*Covid Vaccine\n\n*Covid Isolation\n\n*Covid Guidelines\n\n*Covid Health Service\n\n*Child Covid Care", wxPoint(200, 150), wxSize(200, 300));
+		st5 = new wxStaticText(this, wxID_ANY, "*Covid Test\n\n*Covid Booster Shot\n\n*Covid Vaccine\n\n*Covid Isolation\n\n*Covid Guidelines\n\n*Covid Health Service\n\n*Child Covid Care", wxPoint(200, 150), wxSize(200, 300));
 		st5->SetBackgroundColour(wxColor(234, 221, 202));
-		st6 = new wxStaticText(this, wxID_ANY, "Ophthalmologists Service:\n\n*Vision Care\n\n*Eye Exam\n\n*Vision tests\n\n*Prescribe Glasses\n\n*Contact Lenses\n\n*Monitor Eye Condition\n\n*Dry Eyes\n\n*Glaucoma\n\n*Vision Theraphy\n\n*Provide Low Vision Aid", wxPoint(400, 150), wxSize(200, 300));
+		st6 = new wxStaticText(this, wxID_ANY, "*Vision Care\n\n*Eye Exam\n\n*Vision Tests\n\n*Prescribe Glasses\n\n*Contact Lenses\n\n*Monitor Eye Condition\n\n*Dry Eyes\n\n*Glaucoma\n\n*Vision Theraphy\n\n*Provide Low Vision Aid", wxPoint(400, 150), wxSize(200, 300));
 		st6->SetBackgroundColour(wxColor(115, 147, 179));
-		st7 = new wxStaticText(this, wxID_ANY, "Child Health Service:\n\n*New Family Consulation\n\n*Newborn Care\n\n*Immunizations\n\n*Pediatric Nutritioin\n\n*Physical Exam\n\n*Toddler Care\n\n*FAN Club\n\n*Obesity\n\n*Fevers, Cold, Flu", wxPoint(600, 150), wxSize(200, 300));
+		st7 = new wxStaticText(this, wxID_ANY, "*New Family Consulation\n\n*Newborn Care\n\n*Immunizations\n\n*Pediatric Nutrition\n\n*Physical Exam\n\n*Toddler Care\n\n*FAN Club\n\n*Obesity\n\n*Fevers, Cold, Flu", wxPoint(600, 150), wxSize(200, 300));
 		st7->SetBackgroundColour(wxColor(144, 238, 144));
 		b0 = new wxButton(this, 10012, "Make An Appointment Now", wxPoint(600, 500), wxSize(210, 20));
 		st8 = new wxStaticText(this, wxID_ANY, "We provide much more service than above \nQuestion/Concerns\nEmail Us at: GloriousHospital@GH.com\n Call us at:1(858)965-8888", wxPoint(0, 500), wxSize(400, 100));
@@ -113,12 +119,12 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "TestGUI - EC327", wxPoint(30, 30), 
 	{
 		st13 = new wxStaticText(this, wxID_ANY, "Appointment Form", wxPoint(25, 10), wxSize(400, 50));
 		st13->SetBackgroundColour(wxColor(115, 147, 179));
-		st14 = new wxStaticText(this, wxID_ANY, "Enter A dates (M,T,W Th, F): ", wxPoint(50, 100), wxSize(200, 50));
+		st14 = new wxStaticText(this, wxID_ANY, "Enter a date (S,M,T,W,Th,F,Sat): ", wxPoint(50, 100), wxSize(200, 50));
 		st14->SetBackgroundColour(wxColor(115, 147, 179));
-		m_txt15 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(300, 100), wxSize(150, 50));
-		st15 = new wxStaticText(this, wxID_ANY, "Enter A Location:\n(808 OR BUMC):", wxPoint(50, 200), wxSize(200, 50));
+		chc_date = new wxChoice(this, wxID_ANY, wxPoint(300, 100), wxDefaultSize, 7, dateArray, 0, wxDefaultValidator, "Weekday");
+		st15 = new wxStaticText(this, wxID_ANY, "Enter a Location:\n(808 OR BUMC)", wxPoint(50, 200), wxSize(200, 50));
 		st15->SetBackgroundColour(wxColor(115, 147, 179));
-		m_txt16 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(300, 200), wxSize(150, 50));
+		chc_loc = new wxChoice(this, wxID_ANY, wxPoint(300, 200), wxDefaultSize, 2, locArray, 0, wxDefaultValidator, "Location");
 		st16 = new wxStaticText(this, wxID_ANY, "Enter your name & ID:", wxPoint(50, 300), wxSize(200, 50));
 		st16->SetBackgroundColour(wxColor(115, 147, 179));
 		m_txt17 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(300, 300), wxSize(150, 50));
@@ -130,11 +136,12 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "TestGUI - EC327", wxPoint(30, 30), 
 		m_txt19 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(300, 500), wxSize(150, 50));
 		st19 = new wxStaticText(this, wxID_ANY, "Enter your Service needed:", wxPoint(50, 600), wxSize(200, 50));
 		st19->SetBackgroundColour(wxColor(115, 147, 179));
-		m_txt20 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(300, 600), wxSize(150, 50));
+		chc_serv = new wxChoice(this, wxID_ANY, wxPoint(300, 600), wxDefaultSize, 35, Services, 0, wxDefaultValidator, "Service");
 		m_btn9 = new wxButton(this, 10013, "Enter", wxPoint(500, 600), wxSize(50, 50));
 		m_btn9->SetBackgroundColour(wxColor(115, 147, 179));
 		m_msg3 = new wxMessageDialog(this, "Saved! We will email you the confirmation!", "Results Confirmation", wxOK | wxCENTRE, wxDefaultPosition);
 		m_msg4 = new wxMessageDialog(this, "Please fill all forms.", "Results Confirmation", wxOK | wxCENTRE, wxDefaultPosition);
+		m_msg6 = new wxMessageDialog(this, "Please fill all forms with appropriate entries.", "Results Confirmation", wxOK | wxCENTRE, wxDefaultPosition);
 	}
 	if (global == 7) 
 	{
@@ -196,11 +203,8 @@ void cMain::OnMenu5(wxCommandEvent& evt)
 }
 cMain::~cMain()
 {
-	delete m_btn1;
-	delete m_btn2;
-	delete m_btn3;
-	delete m_text;
-	delete m_msg;
+	delete m_btn1;	delete m_btn2;	delete m_btn3;	delete m_text;	delete m_msg; delete m_btn3; delete m_btn4;	delete m_btn5;	delete m_btn6 ; delete m_btn7; delete m_btn8; delete m_btn9; delete m_text; delete m_text1; delete m_text2; delete m_text3; delete m_text4; delete m_msg1; delete m_msg2; delete m_msg3; delete m_msg4; delete m_msg5; delete m_msg6; delete m_txt1; delete m_txt2; delete m_txt3; delete m_txt4; delete m_txt5; delete m_txt6; delete m_txt7; delete m_txt8; delete m_txt9; delete m_txt10; delete
+		m_txt11; delete m_txt12; delete m_txt13; delete m_txt14; delete m_txt15; delete	m_txt16; delete m_txt17; delete m_txt18; delete m_txt19; delete m_txt20; delete b0; delete st0; delete st1; delete st2; delete st3; delete	st4; delete st5; delete st6; delete st7; delete st8; delete	st9; delete st10; delete st11; delete st12; delete st13; delete st14; delete st15; delete st16; delete st17; delete st18; delete st19; delete t0; delete chc_date; delete chc_loc;
 }
 
 void cMain::OnScheduleButtonClicked(wxCommandEvent& evt)
@@ -249,12 +253,12 @@ void cMain::OnSaveButtonClicked(wxCommandEvent& evt)
 void cMain::OnEClicked(wxCommandEvent& evt)
 {
 	std::ofstream resultsFile("Output_File.txt");
-	if (m_txt4->GetValue().empty() == true || m_txt6->GetValue().empty() == true || m_txt8->GetValue().empty() == true || m_txt10->GetValue().empty() == true || m_txt12->GetValue().empty() == true)
+	if (chc_date->GetSelection() == wxNOT_FOUND || chc_loc->GetSelection() == wxNOT_FOUND || m_txt8->GetValue().empty() == true || m_txt10->GetValue().empty() == true || m_txt12->GetValue().empty() == true)
 		m_msg4->ShowModal();
 	else
 	{
-		resultsFile << "Appointment Date: " << m_txt4->GetValue() << std::endl;
-		resultsFile << "Location:" << m_txt6->GetValue() << std::endl;
+		resultsFile << "Appointment Date: " << chc_date->GetString(chc_date->GetSelection()) << std::endl;
+		resultsFile << "Location: " << chc_loc->GetString(chc_loc->GetSelection()) << std::endl;
 		resultsFile << "Name & ID: " << m_txt8->GetValue() << std::endl;
 		resultsFile << "Email: " << m_txt10->GetValue() << std::endl;
 		resultsFile << "Phone Number: " << m_txt12->GetValue() << std::endl;
@@ -267,18 +271,18 @@ void cMain::OnEClicked(wxCommandEvent& evt)
 void cMain::OnExit2(wxCommandEvent& evt)
 {
 	std::ofstream resultsFile("Output_File.txt");
-	if (m_txt15->GetValue().empty() == true || m_txt16->GetValue().empty() == true || m_txt17->GetValue().empty() == true || m_txt18->GetValue().empty() == true || m_txt19->GetValue().empty() == true || m_txt20->GetValue().empty() == true)
+	if (chc_date->GetSelection() == wxNOT_FOUND || chc_loc->GetSelection() == wxNOT_FOUND || m_txt17->GetValue().empty() == true || m_txt18->GetValue().empty() == true || m_txt19->GetValue().empty() == true || chc_serv->GetSelection() == wxNOT_FOUND)
 	{
 		m_msg4->ShowModal();
 	}
 	else
 	{
-		resultsFile << "Appointment Date: " << m_txt15->GetValue() << std::endl;
-		resultsFile << "Location:" << m_txt16->GetValue() << std::endl;
+		resultsFile << "Appointment Date: " << chc_date->GetString(chc_date->GetSelection()) << std::endl;
+		resultsFile << "Location: " << chc_loc->GetString(chc_loc->GetSelection()) << std::endl;
 		resultsFile << "Name & ID: " << m_txt17->GetValue() << std::endl;
 		resultsFile << "Email: " << m_txt18->GetValue() << std::endl;
 		resultsFile << "Phone Number: " << m_txt19->GetValue() << std::endl;
-		resultsFile << "Service Requested: " << m_txt20->GetValue() << std::endl;
+		resultsFile << "Service Requested: " << chc_serv->GetString(chc_serv->GetSelection()) << std::endl;
 		resultsFile.close();
 		m_msg3->ShowModal();
 		evt.Skip();
